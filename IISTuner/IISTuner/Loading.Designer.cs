26349a1151b31bfa7da8dd882f0f92a87a06e9ba
just keep moving <408720988@qq.com>
2020-01-02 10:52:47 +0800

    init commiet

---
+++ IISTuner/IISTuner/Loading.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace IISTuner
+{
+    partial class Loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Loading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 70);
+            this.Name = "Loading";
+            this.Opacity = 0.1D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartFrom";
+            this.Load += new System.EventHandler(this.StartFrom_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
